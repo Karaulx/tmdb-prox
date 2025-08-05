22321c4ae8b6e42e3add14e3682491bbc5c6507a
Karaulx <ggwpizich@gmail.com>
2025-08-06 01:45:00 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,35 +1,19 @@
-(function() {
-    'use strict';
-    
-    console.log('[TMDB Proxy] Адаптация для Lampa 2.4.6');
-    
-    const CONFIG = {
-        proxy: 'https://novomih25.duckdns.org:9092/3',
-        credentials: {
-            username: 'jackett',
-            password: '3p4uh49y'
-        }
-    };
+const originalRequest = XMLHttpRequest.prototype.open;
+XMLHttpRequest.prototype.open = function(method, url) {
+    if (/themoviedb\.org/.test(url)) {
+        // Удаляем лишний 'https://' при замене
+        const newUrl = url
+            .replace(/https:\/\/api\.themoviedb\.org\/3/, CONFIG.proxy)
+            .replace(/https:\/\/image\.tmdb\.org/, CONFIG.proxy.replace('/3', ''))
+            .replace(/http:\/\/api\.themoviedb\.org\/3/, CONFIG.proxy); // На случай HTTP
 
-    // Перехватчик для Lampa 2.x
-    const originalRequest = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(method, url) {
-        if (/themoviedb\.org/.test(url)) {
-            const newUrl = url
-                .replace(/api\.themoviedb\.org\/3/, CONFIG.proxy)
-                .replace(/image\.tmdb\.org/, CONFIG.proxy.replace('/3', ''));
-            
-            console.log('[TMDB Proxy] Перенаправление:', url, '→', newUrl);
-            
-            // Добавляем авторизацию
-            this.setRequestHeader('Authorization', 'Basic ' + btoa(
-                CONFIG.credentials.username + ':' + CONFIG.credentials.password
-            ));
-            
-            arguments[1] = newUrl; // Подменяем URL
-        }
-        return originalRequest.apply(this, arguments);
-    };
-
-    console.log('[TMDB Proxy] Успешно подключен!');
-})();
+        console.log('[TMDB Proxy] Перенаправление:', url, '→', newUrl);
+        
+        this.setRequestHeader('Authorization', 'Basic ' + btoa(
+            CONFIG.credentials.username + ':' + CONFIG.credentials.password
+        ));
+        
+        arguments[1] = newUrl; // Подменяем URL
+    }
+    return originalRequest.apply(this, arguments);
+};
