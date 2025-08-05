5821d9452f78574fe78bf6ebd2772688d4694f9c
Karaulx <ggwpizich@gmail.com>
2025-08-06 02:37:23 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,6 +1,31 @@
 (function() {
     'use strict';
-    
+    // Вставьте в начало плагина (после 'use strict')
+const replaceImageUrls = (url) => {
+    return url.replace(/https?:\/\/image\.tmdb\.org/, 'https://novomih25.duckdns.org:9092');
+};
+
+// Перехватчик для изображений
+const originalCreateElement = document.createElement;
+document.createElement = function(tagName) {
+    const element = originalCreateElement.call(this, tagName);
+    if (tagName.toLowerCase() === 'img') {
+        const originalSrc = Object.getOwnPropertyDescriptor(HTMLImageElement.prototype, 'src');
+        Object.defineProperty(element, 'src', {
+            get: function() { return originalSrc.get.call(this); },
+            set: function(value) {
+                if (/image\.tmdb\.org/.test(value)) {
+                    const newUrl = replaceImageUrls(value);
+                    console.log('[TMDB Proxy] Замена изображения:', value, '→', newUrl);
+                    originalSrc.set.call(this, newUrl);
+                } else {
+                    originalSrc.set.call(this, value);
+                }
+            }
+        });
+    }
+    return element;
+};
     console.log('[TMDB Proxy] Адаптация для Lampa 2.4.6 (исправлено 500)');
     
     const CONFIG = {
