abb32b363cd94f68937e014749b8167cb868a92f
Karaulx <ggwpizich@gmail.com>
2025-08-06 02:16:36 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     
-    console.log('[TMDB Proxy] Адаптация для Lampa 2.4.6 (финальная)');
+    console.log('[TMDB Proxy] Адаптация для Lampa 2.4.6 (исправлено 500)');
     
     const CONFIG = {
         proxy: 'https://novomih25.duckdns.org:9092/3',
@@ -14,25 +14,20 @@
     const originalRequest = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function(method, url) {
         if (/themoviedb\.org/.test(url)) {
-            const newUrl = url.replace(/https?:\/\/api\.themoviedb\.org\/3/, CONFIG.proxy);
+            const newUrl = url
+                .replace(/https?:\/\/api\.themoviedb\.org\/3/, CONFIG.proxy)
+                .replace(/([?&])api_key=[^&]+&?/, '$1'); // Удаляем API-ключ из URL
+            
             console.log('[TMDB Proxy] Перенаправление:', url, '→', newUrl);
             
             this.setRequestHeader('Authorization', 'Basic ' + btoa(
                 CONFIG.credentials.username + ':' + CONFIG.credentials.password
             ));
             
-            this.addEventListener('load', function() {
-                if (this.status === 200) {
-                    console.log('[TMDB Proxy] Данные получены!');
-                } else {
-                    console.error('[TMDB Proxy] Ошибка:', this.status, this.statusText);
-                }
-            });
-            
             arguments[1] = newUrl;
         }
         return originalRequest.apply(this, arguments);
     };
 
-    console.log('[TMDB Proxy] Готово!');
+    console.log('[TMDB Proxy] Плагин активирован!');
 })();
