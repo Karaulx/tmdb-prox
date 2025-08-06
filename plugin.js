4bf858228031c98cebb07c3cd66d056af0094ce7
Karaulx <ggwpizich@gmail.com>
2025-08-06 03:24:14 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,109 +1,104 @@
 (function() {
     'use strict';
     
-    console.log('[TMDB Proxy] Плагин v12.8 [Stable]');
+    console.log('[TMDB Proxy] Плагин v12.9 [Fixed Images]');
     
-    // Конфигурация (ОБЯЗАТЕЛЬНО ЗАМЕНИТЕ НА СВОИ!)
     const CONFIG = {
         proxy: 'https://novomih25.duckdns.org:9092/3',
         imageProxy: 'https://novomih25.duckdns.org:9092',
         credentials: {
-            username: 'jackett', 
-            password: '3p4uh49y' // Должен совпадать с /etc/nginx/.htpasswd
-        },
-        forceProxy: true // Пропустить проверки
-    };
-
-    // 1. Перехватчик для XMLHttpRequest (основной)
-    const originalXHR = window.XMLHttpRequest;
-    window.XMLHttpRequest = class extends originalXHR {
-        open(method, url) {
-            if (/themoviedb\.org/.test(url)) {
-                const newUrl = url
-                    .replace(/https?:\/\/api\.themoviedb\.org\/3/, CONFIG.proxy)
-                    .replace(/https?:\/\/image\.tmdb\.org/, CONFIG.imageProxy);
-                
-                console.log('[TMDB Proxy] Перенаправление:', url, '→', newUrl);
-                super.open(method, newUrl);
-                this.setRequestHeader('Authorization', 'Basic ' + btoa(
-                    CONFIG.credentials.username + ':' + CONFIG.credentials.password
-                ));
-            } else {
-                super.open(method, url);
-            }
+            username: 'jackett',
+            password: '3p4uh49y'
         }
     };
 
-    // 2. Перехватчик для fetch (на случай, если Lampa использует его)
-    const originalFetch = window.fetch;
-    window.fetch = async (url, options) => {
-        if (/themoviedb\.org/.test(url)) {
-            const newUrl = url
+    // 1. Перехватчик для XMLHttpRequest и fetch
+    const interceptRequests = () => {
+        const originalXHR = window.XMLHttpRequest;
+        window.XMLHttpRequest = class extends originalXHR {
+            open(method, url) {
+                if (/themoviedb\.org/.test(url)) {
+                    url = this._replaceUrl(url);
+                    super.open(method, url);
+                    this.setRequestHeader('Authorization', this._getAuthHeader());
+                } else {
+                    super.open(method, url);
+                }
+            }
+        };
+
+        const originalFetch = window.fetch;
+        window.fetch = async (input, init) => {
+            if (typeof input === 'string' && /themoviedb\.org/.test(input)) {
+                const url = this._replaceUrl(input);
+                const options = init || {};
+                options.headers = {
+                    ...options.headers,
+                    'Authorization': this._getAuthHeader()
+                };
+                return originalFetch(url, options);
+            }
+            return originalFetch(input, init);
+        };
+    };
+
+    // 2. Перехватчик для изображений
+    const interceptImages = () => {
+        const originalCreateElement = document.createElement;
+        document.createElement = function(tagName, options) {
+            const element = originalCreateElement.call(this, tagName, options);
+            if (tagName.toLowerCase() === 'img') {
+                const descriptor = Object.getOwnPropertyDescriptor(HTMLImageElement.prototype, 'src');
+                Object.defineProperty(element, 'src', {
+                    set: function(value) {
+                        if (/image\.tmdb\.org/.test(value)) {
+                            const newUrl = value.replace(
+                                /https?:\/\/image\.tmdb\.org/,
+                                CONFIG.imageProxy
+                            );
+                            console.log('[TMDB Proxy] Замена изображения:', newUrl);
+                            descriptor.set.call(this, newUrl);
+                        } else {
+                            descriptor.set.call(this, value);
+                        }
+                    },
+                    get: function() {
+                        return descriptor.get.call(this);
+                    }
+                });
+            }
+            return element;
+        };
+    };
+
+    // 3. Вспомогательные методы
+    const helpers = {
+        _replaceUrl: function(url) {
+            return url
                 .replace(/https?:\/\/api\.themoviedb\.org\/3/, CONFIG.proxy)
                 .replace(/https?:\/\/image\.tmdb\.org/, CONFIG.imageProxy);
-            
-            console.log('[TMDB Proxy] Перехват fetch:', newUrl);
-            return originalFetch(newUrl, {
-                ...options,
-                headers: {
-                    ...options?.headers,
-                    'Authorization': 'Basic ' + btoa(
-                        CONFIG.credentials.username + ':' + CONFIG.credentials.password
-                    )
-                }
-            });
+        },
+        _getAuthHeader: function() {
+            return 'Basic ' + btoa(CONFIG.credentials.username + ':' + CONFIG.credentials.password);
         }
-        return originalFetch(url, options);
-    };
-
-    // 3. Перехватчик для динамических изображений
-    const originalCreateElement = document.createElement;
-    document.createElement = function(tag) {
-        const el = originalCreateElement.apply(this, arguments);
-        if (tag.toLowerCase() === 'img') {
-            const originalSrc = Object.getOwnPropertyDescriptor(HTMLImageElement.prototype, 'src');
-            Object.defineProperty(el, 'src', {
-                set: function(value) {
-                    if (/image\.tmdb\.org/.test(value)) {
-                        const newUrl = value.replace(
-                            'https://image.tmdb.org', 
-                            CONFIG.imageProxy
-                        );
-                        console.log('[TMDB Proxy] Замена изображения:', newUrl);
-                        originalSrc.set.call(this, newUrl);
-                    } else {
-                        originalSrc.set.call(this, value);
-                    }
-                },
-                get: function() {
-                    return originalSrc.get.call(this);
-                }
-            });
-        }
-        return el;
     };
 
     // 4. Инициализация
-    function init() {
-        console.log('[TMDB Proxy] Успешно активирован!');
+    const init = () => {
+        interceptRequests();
+        interceptImages();
         
-        // Для Lampa 4.x+ (если доступен interceptor)
+        // Для Lampa 4.x+
         if (window.lampa?.interceptor?.request?.add) {
             lampa.interceptor.request.add({
                 before: req => {
                     if (/themoviedb\.org/.test(req.url)) {
-                        const newUrl = req.url
-                            .replace(/api\.themoviedb\.org\/3/, CONFIG.proxy)
-                            .replace(/image\.tmdb\.org/, CONFIG.imageProxy);
-                        
                         return {
                             ...req,
-                            url: newUrl,
+                            url: helpers._replaceUrl(req.url),
                             headers: {
                                 ...req.headers,
-                                'Authorization': 'Basic ' + btoa(
-                                    CONFIG.credentials.username + ':' + CONFIG.credentials.password
-                                )
+                                'Authorization': helpers._getAuthHeader()
                             }
                         };
                     }
@@ -111,7 +106,9 @@
                 }
             });
         }
-    }
+        
+        console.log('[TMDB Proxy] Успешно активирован!');
+    };
 
     // Автозапуск
     if (document.readyState === 'complete') {
