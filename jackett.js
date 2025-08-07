1b2749ec0e3254e6807a59e8494a09773da76e80
Karaulx <ggwpizich@gmail.com>
2025-08-07 06:58:50 +0300

    Create jackett.js

---
+++ jackett.js
@@ -0,0 +1,79 @@
+(function() {
+    'use strict';
+    
+    // ================= CORS FIXER =================
+    const fixUrl = url => url.replace(/([^:]\/)\/+/g, '$1');
+    
+    // Сохраняем оригинальные методы
+    window._originalFetch = window.fetch;
+    window._originalXHROpen = XMLHttpRequest.prototype.open;
+    
+    // Перехватчик fetch
+    window.fetch = async function(input, init) {
+        try {
+            input = typeof input === 'string' ? fixUrl(input) : input;
+            return await window._originalFetch(input, {
+                ...init,
+                mode: 'no-cors',
+                headers: {
+                    ...(init?.headers || {}),
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            });
+        } catch (error) {
+            console.error('[Fetch Error]', error);
+            throw error;
+        }
+    };
+    
+    // Перехватчик XMLHttpRequest
+    XMLHttpRequest.prototype.open = function() {
+        if (arguments[1]) {
+            arguments[1] = fixUrl(arguments[1]);
+        }
+        this.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        return window._originalXHROpen.apply(this, arguments);
+    };
+    
+    // ================= JACKETT INTEGRATION =================
+    Lampa.Jackett = {
+        getConfig: function() {
+            return {
+                server: Lampa.Storage.get('jackett_server') || 'http://novomih25.duckdns.org',
+                apiKey: Lampa.Storage.get('jackett_apikey') || '',
+                timeout: 10000
+            };
+        },
+        
+        search: function(params) {
+            const config = this.getConfig();
+            if (!config.apiKey) {
+                console.error('Jackett API key not configured');
+                return Promise.reject('API key required');
+            }
+            
+            const url = new URL(`${config.server}/api/v2.0/indexers/all/results`);
+            
+            // Базовые параметры
+            url.searchParams.append('apikey', config.apiKey);
+            
+            // Добавляем пользовательские параметры
+            Object.keys(params).forEach(key => {
+                if (Array.isArray(params[key])) {
+                    params[key].forEach(val => url.searchParams.append(`${key}[]`, val));
+                } else {
+                    url.searchParams.append(key, params[key]);
+                }
+            });
+            
+            return fetch(url.toString(), {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            }).then(r => r.json());
+        }
+    };
+    
+    console.log('[Lampa Mod] Jackett+CORS fixer activated');
+})();
