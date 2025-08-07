2e7cd618c19c1e79e43576bf9fac610b801e026c
Karaulx <ggwpizich@gmail.com>
2025-08-07 07:23:51 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -1,33 +1,41 @@
-// cors-fixer.js
 (function() {
     'use strict';
     
     const fixUrl = url => url.replace(/([^:]\/)\/+/g, '$1');
 
-    // Сохраняем оригинальные методы
-    window._originalFetch = window.fetch;
-    window._originalXHROpen = XMLHttpRequest.prototype.open;
-    
-    // Fetch interceptor
+    // 1. Перехват fetch с обработкой ошибок
+    const _originalFetch = window.fetch;
     window.fetch = async function(input, init) {
         try {
             input = typeof input === 'string' ? fixUrl(input) : input;
-            const response = await window._originalFetch(input, {
+            
+            const response = await _originalFetch.call(this, input, {
                 ...init,
-                mode: 'no-cors'
+                mode: 'no-cors',
+                headers: {
+                    ...(init?.headers || {}),
+                    'Content-Type': 'application/json'
+                }
             });
+            
+            if (!response.ok) throw new Error('Network error');
             return response;
+            
         } catch (error) {
             console.error('Fetch error:', error);
             throw error;
         }
     };
 
-    // XMLHttpRequest interceptor
-    XMLHttpRequest.prototype.open = function() {
-        arguments[1] = fixUrl(arguments[1]);
-        return window._originalXHROpen.apply(this, arguments);
-    };
+    // 2. Перехват XMLHttpRequest
+    if (window.XMLHttpRequest) {
+        const _originalOpen = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function() {
+            arguments[1] = fixUrl(arguments[1]);
+            this.addEventListener('error', () => console.error('XHR error'));
+            return _originalOpen.apply(this, arguments);
+        };
+    }
 
-    console.log('[CORS Fixer] Activated');
+    console.log('[Network Fixer] Activated');
 })();
