c9db7b8571a9cda17483204e9d718347b15b3a95
Karaulx <ggwpizich@gmail.com>
2025-08-11 02:13:36 +0300

    Update rh.js

--- rh.js
+++ rh.js
@@ -1,11 +1,12 @@
-(function (  ) {
+(function() {
     'use strict';
 
-    function startPlugin() {
-      window.ReYohohoFixed = true;
+    // Защита от повторной загрузки
+    if (window.ReYohohoPluginReady) return;
+    window.ReYohohoPluginReady = true;
 
-    waitForLampa(function() {
-        // ТОЧНАЯ КОПИЯ вашего первоначального кода ▼▼▼
+    function initPlugin() {
+        // Основная функция воспроизведения (ваш оригинальный код)
         async function handleReYohohoPlay(data) {
             const movie = data.movie;
             const type = movie.name ? 'tv' : 'movie';
@@ -14,6 +15,7 @@
                 const contentUrl = `https://reyohoho.github.io/${type}/${movie.tmdb_id || movie.kinopoisk_id}`;
                 const html = await fetch(contentUrl).then(r => r.text());
                 const videoUrl = extractVideoUrl(html);
+                
                 if (!videoUrl) throw new Error('Ссылка на видео не найдена');
                 
                 Lampa.Player.play(videoUrl, {
@@ -42,15 +44,62 @@
             const match = html.match(regex);
             return match ? match[0] : null;
         }
-        // ▲▲▲ Код выше полностью идентичен вашему первоначальному варианту
 
-  if (window.appready) add(); else {
-        Lampa.Listener.follow('app', function (e) {
-          if (e.type == 'ready') { add(); }
+        // Добавляем кнопку в интерфейс (аналогично torrents)
+        Lampa.Listener.follow('full', function(e) {
+            if (e.type == 'complite') {
+                const button = `
+                    <div class="full-start__button view--reyohoho">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                            <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/>
+                        </svg>
+                        <span>ReYohoho</span>
+                    </div>
+                `;
+                
+                const btn = $(button);
+                btn.on('hover:enter', function() {
+                    handleReYohohoPlay(e.data);
+                });
+                
+                if (e.data && e.object) {
+                    e.object.activity.render().find('.full-start__buttons').append(btn);
+                }
+            }
         });
-      }
+
+        // Регистрируем обработчик плеера
+        Lampa.Player.addHandler({
+            name: 'reyohoho',
+            title: 'ReYohoho',
+            priority: 10,
+            handler: handleReYohohoPlay
+        });
+
+        // Добавляем настройки (если нужно)
+        Lampa.SettingsApi.addParam({
+            component: 'reyohoho',
+            param: {
+                name: 'reyohoho_enabled',
+                type: 'select',
+                values: ['true', 'false'],
+                value: 'true'
+            },
+            field: {
+                name: 'Включить ReYohoho',
+                description: 'Активировать источник ReYohoho'
+            }
+        });
     }
 
-    if (!window.plugin_torrents_ready) startPlugin();
-
-})( );
+    // Запускаем плагин
+    if (window.appready) {
+        initPlugin();
+    } else {
+        Lampa.Listener.follow('app', function(e) {
+            if (e.type == 'ready') {
+                initPlugin();
+            }
+        });
+    }
+})();
