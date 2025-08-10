408ea5d8bc0d0c8e9389c07b07e5ff6e350560c3
Karaulx <ggwpizich@gmail.com>
2025-08-11 00:09:08 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -5,17 +5,7 @@
         window.plugin_reyohoho_ready = true;
 
         function add() {
-            function button_click(data) {
-                Lampa.Activity.push({
-                    url: '',
-                    title: 'ReYohoho',
-                    component: 'reyohoho',
-                    movie: data.movie,
-                    type: data.movie.name ? 'tv' : 'movie'
-                });
-            }
-
-            // Добавляем кнопку на карточку фильма/сериала
+            // Добавляем кнопку просмотра
             Lampa.Listener.follow('full', function(e) {
                 if (e.type == 'complite') {
                     var button = `
@@ -28,57 +18,51 @@
                     
                     var btn = $(button);
                     btn.on('hover:enter', function() {
-                        button_click(e.data);
-                    });
-                    
-                    if (e.data && e.object) {
-                        e.object.activity.render().find('.view--torrent').last().after(btn);
-                    }
-                }
-            });
-
-            // Регистрируем компонент для обработки просмотра
-            Lampa.Component.add('reyohoho', {
-                create: function() {
-                    var network = new Lampa.Reguest();
-                    var scroll = new Lampa.Scroll({mask: true, over: true});
-                    var files = new Lampa.Explorer(this);
-                    
-                    this.loading = function(status) {
-                        if (status) files.loading(true);
-                        else files.loading(false);
-                    };
-
-                    this.start = function() {
-                        var movie = this.movie;
-                        var type = this.type;
-                        
-                        this.loading(true);
+                        var movie = e.data.movie;
+                        var type = movie.name ? 'tv' : 'movie';
                         
                         // Формируем URL для ReYohoho
                         var url = 'https://reyohoho.github.io/player.html?' + 
                                   'id=' + (movie.kinopoisk_id || movie.tmdb_id) + 
                                   '&type=' + type;
                         
+                        // Для сериалов добавляем параметры
                         if (type === 'tv') {
                             url += '&season=1&episode=1';
                         }
                         
-                        // Открываем в плеере Lampa
-                        Lampa.Player.play(url, {
-                            title: movie.title || movie.name,
-                            external: false,
-                            headers: {
-                                'Referer': 'https://reyohoho.github.io/',
-                                'Origin': 'https://reyohoho.github.io'
-                            }
-                        });
-                        
-                        this.loading(false);
-                        Lampa.Activity.backward();
-                    };
+                        // Получаем ссылку на видео
+                        fetch(url)
+                            .then(response => response.text())
+                            .then(html => {
+                                // Парсим HTML чтобы найти m3u8 ссылку
+                                var parser = new DOMParser();
+                                var doc = parser.parseFromString(html, 'text/html');
+                                var videoUrl = doc.querySelector('video source')?.src;
+                                
+                                if (videoUrl) {
+                                    // Запускаем плеер
+                                    Lampa.Player.play(videoUrl, {
+                                        title: movie.title || movie.name,
+                                        external: false,
+                                        headers: {
+                                            'Referer': 'https://reyohoho.github.io/',
+                                            'Origin': 'https://reyohoho.github.io'
+                                        }
+                                    });
+                                } else {
+                                    Lampa.Noty.show('Не удалось получить ссылку на видео');
+                                }
+                            })
+                            .catch(error => {
+                                console.error('ReYohoho error:', error);
+                                Lampa.Noty.show('Ошибка при получении данных');
+                            });
+                    });
                     
-                    return files.render();
+                    if (e.data && e.object) {
+                        e.object.activity.render().find('.view--torrent').last().after(btn);
+                    }
                 }
             });
         }
