d38f08ea52f4163709d570239be175704e50f27b
Karaulx <ggwpizich@gmail.com>
2025-08-11 01:19:42 +0300

    Update rh.js

--- rh.js
+++ rh.js
@@ -7,7 +7,14 @@
         function add() {
             // Добавляем кнопку в интерфейс
             Lampa.Listener.follow('full', function(e) {
-                if (e.type == 'complite') {
+                if (e.type == 'complite' && e.data && e.data.movie) {
+                    // Проверяем наличие ID перед созданием кнопки
+                    const movie = e.data.movie;
+                    if (!movie.tmdb_id && !movie.kinopoisk_id) {
+                        console.log('ReYohoho: Пропуск - нет ID контента');
+                        return;
+                    }
+
                     var button = `
                         <div class="full-start__button view--reyohoho">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
@@ -18,50 +25,60 @@
                     
                     var btn = $(button);
                     btn.on('hover:enter', function() {
-                        handlePlayback(e.data.movie);
+                        handlePlayback(movie);
                     });
                     
-                    if (e.data && e.object) {
-                        e.object.activity.render().find('.view--torrent').last().after(btn);
-                    }
+                    e.object.activity.render().find('.view--torrent').last().after(btn);
                 }
             });
 
             // Основная функция воспроизведения
-            async function handlePlayback(movie) {
+            function handlePlayback(movie) {
                 try {
-                    if (!movie) throw new Error('Нет данных о фильме');
-                    
+                    if (!movie) {
+                        Lampa.Noty.show('Ошибка: нет данных о фильме');
+                        return;
+                    }
+
                     const id = movie.tmdb_id || movie.kinopoisk_id;
-                    if (!id) throw new Error('ID контента не найден');
-                    
+                    if (!id) {
+                        Lampa.Noty.show('Ошибка: ID контента не найден');
+                        return;
+                    }
+
                     const type = movie.name ? 'tv' : 'movie';
-                    const playerUrl = buildPlayerUrl(id, type, movie);
-                    
-                    Lampa.Player.play(playerUrl, {
-                        title: movie.title || movie.name,
-                        external: false,
-                        headers: {
-                            'Referer': 'https://reyohoho.github.io/',
-                            'Origin': 'https://reyohoho.github.io'
-                        }
-                    });
+                    const playerUrl = `https://reyohoho.github.io/player.html?` + 
+                                     `id=${id}&type=${type}` + 
+                                     (type === 'tv' ? '&season=1&episode=1' : '');
+
+                    // Запускаем плеер с проверкой URL
+                    if (isValidUrl(playerUrl)) {
+                        Lampa.Player.play(playerUrl, {
+                            title: movie.title || movie.name,
+                            external: false,
+                            headers: {
+                                'Referer': 'https://reyohoho.github.io/',
+                                'Origin': 'https://reyohoho.github.io'
+                            }
+                        });
+                    } else {
+                        Lampa.Noty.show('Некорректный URL для воспроизведения');
+                    }
                     
                 } catch (error) {
                     console.error('ReYohoho playback error:', error);
-                    Lampa.Noty.show('Ошибка запуска: ' + (error.message || 'неизвестная ошибка'));
+                    Lampa.Noty.show('Ошибка запуска плеера');
                 }
             }
 
-            // Формирование URL плеера
-            function buildPlayerUrl(id, type, movie) {
-                let url = `https://reyohoho.github.io/player.html?id=${id}&type=${type}`;
-                
-                if (type === 'tv') {
-                    url += `&season=1&episode=1`;
+            // Проверка валидности URL
+            function isValidUrl(url) {
+                try {
+                    new URL(url);
+                    return true;
+                } catch (e) {
+                    return false;
                 }
-                
-                return url;
             }
         }
 
