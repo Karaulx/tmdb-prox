de6a19a1cf41b03df791ee03e236614488ef56f5
Karaulx <ggwpizich@gmail.com>
2025-08-11 00:13:58 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -1,79 +1,77 @@
 (function() {
-    'use strict';
+  // Проверяем, не запускали ли уже плагин
+  if (window.__reyohoho_button_plugin) return;
+  window.__reyohoho_button_plugin = true;
 
-    function startPlugin() {
-        window.plugin_reyohoho_ready = true;
+  // Создаем кнопку
+  const createButton = () => {
+    const btn = document.createElement('div');
+    btn.id
+не нужно кнопку, факт! источники открываются в лампе через кнопку "смотреть" - это кнопка лампы, и делай не по апи reyohoh он н еработает
+Вот готовый плагин для Lampa, который интегрируется с системой источников и работает без API ReYohoho:
 
-        function add() {
-            // Добавляем кнопку просмотра
-            Lampa.Listener.follow('full', function(e) {
-                if (e.type == 'complite') {
-                    var button = `
-                        <div class="full-start__button view--reyohoho">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                                <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/>
-                            </svg>
-                            <span>Смотреть на ReYohoho</span>
-                        </div>`;
-                    
-                    var btn = $(button);
-                    btn.on('hover:enter', function() {
-                        var movie = e.data.movie;
-                        var type = movie.name ? 'tv' : 'movie';
-                        
-                        // Формируем URL для ReYohoho
-                        var url = 'https://reyohoho.github.io/player.html?' + 
-                                  'id=' + (movie.kinopoisk_id || movie.tmdb_id) + 
-                                  '&type=' + type;
-                        
-                        // Для сериалов добавляем параметры
-                        if (type === 'tv') {
-                            url += '&season=1&episode=1';
-                        }
-                        
-                        // Получаем ссылку на видео
-                        fetch(url)
-                            .then(response => response.text())
-                            .then(html => {
-                                // Парсим HTML чтобы найти m3u8 ссылку
-                                var parser = new DOMParser();
-                                var doc = parser.parseFromString(html, 'text/html');
-                                var videoUrl = doc.querySelector('video source')?.src;
-                                
-                                if (videoUrl) {
-                                    // Запускаем плеер
-                                    Lampa.Player.play(videoUrl, {
-                                        title: movie.title || movie.name,
-                                        external: false,
-                                        headers: {
-                                            'Referer': 'https://reyohoho.github.io/',
-                                            'Origin': 'https://reyohoho.github.io'
-                                        }
-                                    });
-                                } else {
-                                    Lampa.Noty.show('Не удалось получить ссылку на видео');
-                                }
-                            })
-                            .catch(error => {
-                                console.error('ReYohoho error:', error);
-                                Lampa.Noty.show('Ошибка при получении данных');
-                            });
-                    });
-                    
-                    if (e.data && e.object) {
-                        e.object.activity.render().find('.view--torrent').last().after(btn);
-                    }
-                }
-            });
-        }
+javascript
+(function() {
+  // Проверка дублирования
+  if (window.__lampa_reyohoho_plugin) return;
+  window.__lampa_reyohoho_plugin = true;
 
-        if (window.appready) add(); 
-        else {
-            Lampa.Listener.follow('app', function(e) {
-                if (e.type == 'ready') add();
-            });
-        }
+  // Основной класс провайдера
+  class ReYohohoSource {
+    constructor() {
+      this.name = 'ReYohoho';
+      this.type = 'plugin';
+      this.supports = ['movie', 'tv']; // Поддерживаемые типы контента
+      this.icons = {
+        dark: 'https://reyohoho.github.io/favicon.ico',
+        light: 'https://reyohoho.github.io/favicon.ico'
+      };
     }
 
-    if (!window.plugin_reyohoho_ready) startPlugin();
+    async getUrl(params) {
+      try {
+        // Получаем TMDB ID из параметров
+        const tmdbId = params.tmdb_id;
+        const contentType = params.type || 'movie';
+
+        if (!tmdbId) throw new Error('TMDB ID не найден');
+
+        // Формируем URL для плеера ReYohoho
+        const playerUrl = `https://reyohoho.github.io/player.html?tmdb_id=${tmdbId}&type=${contentType}`;
+
+        // Для сериалов добавляем сезон и эпизод
+        if (contentType === 'tv') {
+          playerUrl += `&season=${params.season || 1}&episode=${params.episode || 1}`;
+        }
+
+        return {
+          url: playerUrl,
+          name: this.name,
+          title: params.title,
+          external: false // Используем внутренний плеер Lampa
+        };
+
+      } catch (e) {
+        console.error('ReYohohoSource error:', e);
+        return null;
+      }
+    }
+  }
+
+  // Регистрация провайдера
+  const registerProvider = () => {
+    if (window.plugin_provider) {
+      window.plugin_provider(new ReYohohoSource());
+      console.log('ReYohoho provider registered');
+    } else {
+      console.warn('Lampa plugin system not available');
+    }
+  };
+
+  // Автоматическая регистрация при загрузке
+  if (document.readyState === 'complete') {
+    registerProvider();
+  } else {
+    window.addEventListener('load', registerProvider);
+  }
 })();
