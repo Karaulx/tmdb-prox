373168101f4af4487d3eec5a9066d0c8ae1453c9
Karaulx <ggwpizich@gmail.com>
2025-08-11 01:31:48 +0300

    Update rh.js

--- rh.js
+++ rh.js
@@ -1,77 +1,69 @@
 (function() {
     'use strict';
+    
+    // 1. Проверка дублирования
+    if (window.__reyohoho_fix_provider) return;
+    window.__reyohoho_fix_provider = true;
 
-    function startPlugin() {
-        window.plugin_reyohoho_ready = true;
-
-        function add() {
-            // Добавляем кнопку в интерфейс
-            Lampa.Listener.follow('full', function(e) {
-                if (e.type == 'complite') {
-                    var button = `
-                        <div class="full-start__button view--reyohoho">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                                <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/>
-                            </svg>
-                            <span>Смотреть на ReYohoho</span>
-                        </div>`;
-                    
-                    var btn = $(button);
-                    btn.on('hover:enter', function() {
-                        handlePlayback(e.data.movie);
-                    });
-                    
-                    if (e.data && e.object) {
-                        e.object.activity.render().find('.view--torrent').last().after(btn);
-                    }
-                }
-            });
-
-            // Функция воспроизведения
-            function handlePlayback(movie) {
+    // 2. Регистрация источника
+    function registerProvider() {
+        const provider = {
+            name: "ReYohoho",
+            type: "movie,tv",
+            icon: "https://reyohoho.github.io/favicon.ico",
+            getUrl: function(params) {
                 try {
-                    const id = movie.tmdb_id || movie.kinopoisk_id;
+                    // Получаем ID из параметров
+                    const id = params.tmdb_id || params.kinopoisk_id;
                     if (!id) {
-                        Lampa.Noty.show('Ошибка: ID контента не найден');
-                        return;
+                        console.error("ReYohoho: ID контента не найден");
+                        return null;
                     }
 
-                    const type = movie.name ? 'tv' : 'movie';
-                    let playerUrl = `https://reyohoho.github.io/player.html?id=${id}&type=${type}`;
+                    // Формируем URL
+                    const type = params.type || (params.movie?.name ? 'tv' : 'movie');
+                    let url = `https://reyohoho.github.io/player.html?id=${id}&type=${type}`;
                     
                     if (type === 'tv') {
-                        playerUrl += `&season=1&episode=1`;
+                        url += `&season=${params.season || 1}&episode=${params.episode || 1}`;
                     }
 
-                    // Запускаем плеер
-                    Lampa.Player.play(playerUrl, {
-                        title: movie.title || movie.name,
+                    return {
+                        url: url,
+                        name: "ReYohoho",
+                        title: params.title || "ReYohoho",
                         external: false,
                         headers: {
-                            'Referer': 'https://reyohoho.github.io/',
-                            'Origin': 'https://reyohoho.github.io'
-                        },
-                        params: {
-                            tmdb_id: movie.tmdb_id,
-                            kinopoisk_id: movie.kinopoisk_id
+                            "Referer": "https://reyohoho.github.io/",
+                            "Origin": "https://reyohoho.github.io"
                         }
-                    });
+                    };
 
-                } catch (error) {
-                    console.error('ReYohoho error:', error);
-                    Lampa.Noty.show('Ошибка запуска плеера');
+                } catch (e) {
+                    console.error("ReYohoho error:", e);
+                    return null;
                 }
             }
-        }
+        };
 
-        // Инициализация
-        if (window.appready) add(); 
-        else {
-            Lampa.Listener.follow('app', function(e) {
-                if (e.type == 'ready') add();
+        // Для Lampa 2.4.6
+        if (typeof window.extensions_provider !== 'undefined') {
+            window.extensions_provider.push(provider);
+            console.log("ReYohoho добавлен в источники");
+        }
+        // Для новых версий
+        else if (typeof Lampa !== 'undefined' && Lampa.Plugins) {
+            Lampa.Plugins.add({
+                name: provider.name,
+                component: provider
             });
         }
     }
 
-    if (!window.plugin_reyohoho_ready) startPlugin();
+    // 3. Запуск
+    if (document.readyState === 'complete') {
+        registerProvider();
+    } else {
+        window.addEventListener('load', registerProvider);
+    }
 })();
