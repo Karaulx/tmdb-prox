1add74718ae781318528b457be069b906a4a0eaf
Karaulx <ggwpizich@gmail.com>
2025-08-13 04:55:06 +0300

    Update rh.js

--- rh.js
+++ rh.js
@@ -1,23 +1,25 @@
 function addReYohohoButton() {
-    // 1. Находим контейнер с трейлерами
-    const trailerContainer = $('.selectbox-body .scroll_content');
+    // 1. Находим контейнер с элементами трейлеров
+    const trailersContainer = $('.scroll__content');
     
-    if (!trailerContainer.length) {
+    if (!trailersContainer.length) {
         console.error('Контейнер трейлеров не найден!');
         return;
     }
 
-    // 2. Создаем кнопку ReYohoho в стиле других элементов
+    // 2. Создаем кнопку ReYohoho в едином стиле
     const reYohohoButton = $(`
-        <div class="selectbox-item selection-item-icon selector re-yohoho-button" 
-             style="border-left: 3px solid #00ff00;">
-            <div class="selectbox-item_icon">
+        <div class="selectbox-item selectbox-item--icon selector re-yohoho-button" 
+             style="border-left: 3px solid #00ff00; margin-bottom: 15px;">
+            <div class="selectbox-item__icon">
                 <svg width="40" height="40" viewBox="0 0 24 24" fill="#00ff00">
                     <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM10 16.5L5.5 12L7 10.5L10 13.5L17 6.5L18.5 8L10 16.5Z"/>
                 </svg>
             </div>
-            <div class="selectbox-item_title">ReYohoho</div>
-            <div class="selectbox-item_subtitle">Альтернативный плеер</div>
+            <div>
+                <div class="selectbox-item__title" style="color: #00ff00">ReYohoho</div>
+                <div class="selectbox-item__subtitle">Альтернативный плеер</div>
+            </div>
         </div>
     `);
 
@@ -25,20 +27,25 @@ function addReYohohoButton() {
     reYohohoButton.on('hover:enter', function() {
         console.log('ReYohoho запущен');
         // Ваш код для запуска ReYohoho
+        // Например: Lampa.Player.play(reYohohoUrl);
     });
 
-    // 4. Вставляем кнопку в начало списка
-    trailerContainer.prepend(reYohohoButton);
-    console.log('Кнопка ReYohoho добавлена');
+    // 4. Вставляем кнопку ПЕРВОЙ в списке
+    trailersContainer.prepend(reYohohoButton);
+    console.log('✅ Кнопка ReYohoho добавлена в начало списка');
+
+    // 5. Временно подсвечиваем контейнер для проверки (удалите в финальной версии)
+    trailersContainer.css('outline', '2px dashed rgba(0,255,0,0.5)');
 }
 
-// Инициализация при загрузке плеера
+// Автоматическая инициализация
 if (window.appready) {
     addReYohohoButton();
 } else {
     Lampa.Listener.follow('app', function(e) {
         if (e.type === 'ready') {
-            setTimeout(addReYohohoButton, 500);
+            // Задержка для полной загрузки интерфейса
+            setTimeout(addReYohohoButton, 800);
         }
     });
 }
